d3bbbc28c4c5c1166b47595b6d59b917a6b23a97
Noel Teo <teo_mou_yi_noel@students.edu.sg>
2026-06-10 19:35:02 +0800

    Commented version

--- module1.py
+++ module1.py
@@ -1,97 +1,70 @@
 # Module 1: Table Counter and Table Zones
 
-# Calculate available tables (used to display number of tables)
-def total_available_tables(zones):
-
-    total = 0
-
-    for zone in zones:
-
-        for table_size in zones[zone]:
-
-            # Add available tables to total
-            total += zones[zone][table_size]
-
-    return total
-
 # Display the number of tables 
 def display_tables(zones):
+    print("Welcome to Smart Hawker Centre") # Welcome message to the user/customer
 
-    print("Welcome to Smart Hawker Centre")
-
-    best_zone_name = ""
+    # Used to calculate the most vacant (best) zone
+    best_zone_name = "" 
     best_zone_count = 0
 
+    total = 0 # calculates the grand total of tables in the hawker centre
+
+    # These nested for loops calculate the "best" zone
     for zone in zones:
-
-        zone_total = 0
-
+        zone_total = 0 # calculates the total of each zone
+        
         for table_size in zones[zone]:
-
             # Sum all available tables in this zone
             zone_total += zones[zone][table_size]
+        print(f"Zone {zone}: {zone_total} tables available") # Print the number of vacant seats for each zone.
+                                                             # e.g. Zone A: 10 tables available
+                                                             #      Zone B: 20 tables available
+       
+       # add the total of each zone to the grand total
+        total += zone_total 
 
-        print(f"Zone {zone}: {zone_total} tables available")
+        # this replicates a max() function to find the max number of vacant seats
+        if zone_total > best_zone_count: # if the current number of vacant seats > current best,
+            best_zone_count = zone_total # set the best to the current number of vacant seats,
+            best_zone_name = zone  # and set the best zone to the corresponding zone.
 
-        if zone_total > best_zone_count:
-            best_zone_count = zone_total
-            best_zone_name = zone
-
-    total = total_available_tables(zones)
+    # Print total number of available tables and the "best" zone
     print(f"Total available tables: {total} ")
-
     print(f"Recommended Zone: {best_zone_name}")
 
 # Occupy a table
-def occupy_table(zones, zone, table_size):
-
+# The function requires the zones dictionary, the user's preferred zone and their table size.
+def occupy_table(zones, zone, table_size): 
+    # These if-statements prevents the user from trying to trick the system
     if zone not in zones:
-
         print("Invalid zone.")
-        return
-
+        return # ends the function
     if table_size not in zones[zone]:
-
         print("Invalid table size.")
-        return
+        return # ends the function
 
-    # First select the zone
-    # Then select the table size inside that zone
-    # Example:
-    # zones["A"][4]
-    # means available 4-seater tables in Zone A
-
-    if zones[zone][table_size] > 0:
-
-        zones[zone][table_size] -= 1
-
-        print(f"Customer seated at {table_size}-seater in Zone {zone}")
-
+    # Check if table size has vacancy
+    if zones[zone][table_size] > 0: # if there is vacant seats for that table size, allow the customer to take their seat
+        zones[zone][table_size] -= 1 # and minus one vacancy
+        print(f"Welcome in to Smart Hawker Centre! \nZone: {zone} \nTable size: {table_size}") # welcome message to customer
     else:
-
-        print(f"No {table_size}-seater available in zone {zone}")
+        print(f"No table for {table_size} available in zone {zone}.") # otherwise print error
 
 # Unoccupy a table
+# The function requires the zones dictionary, the fixed max tables dictionary, the customer's table details (zone + table size)
 def unoccupy_table(zones, max_tables, zone, table_size):
-
+    # These if-statements prevents the user from trying to trick the system
     if zone not in zones:
-
         print("Invalid zone.")
         return
-
     if table_size not in zones[zone]:
-
         print("Invalid table size.")
         return
 
     # Ensure count never exceeds original amount
-
-    if zones[zone][table_size] < max_tables[zone][table_size]:
-
-        zones[zone][table_size] += 1
-
-        print(f"{table_size}-seater in zone {zone} is now vacant")
-
+    if zones[zone][table_size] < max_tables[zone][table_size]: # if the current number of tables is less than the total number of tables,
+        zones[zone][table_size] += 1 # add one vacancy to and successfully let customer out
+        print(f"Thank you for visiting Smart Hawker Centre!")
     else:
-
-        print("Error: Count cannot exceed maximum.")
+        print("Error: Count cannot exceed maximum.") # otherwise print error
