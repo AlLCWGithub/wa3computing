a09f6aa298df9495559637ecb71c8ba64fb56a63
AlLCWGithub <alson20110204@gmail.com>
2026-08-05 14:42:37 +0800

    Updated Repo (Please everyone give me money I broke i want go china eat food

--- microbit_main.py
+++ microbit_main.py
@@ -1,4 +1,7 @@
 from microbit import *
+import distance
+
+distance_sensor = distance.DISTANCE(pin1)
 
 uart.init(baudrate=9600)
 
@@ -6,50 +9,60 @@ buffer = ""
 
 display.scroll("READY")
 
-while True:
 
+def serialconnection():
     data = uart.read() 
-
+    
     if data:
-
+    
         buffer += data.decode()
-
+    
         while "\n" in buffer:
-
+    
             line, buffer = buffer.split("\n", 1)
-
+    
             line = line.strip()
-
+    
             if "|" not in line:
                 uart.write("ERROR|Invalid packet\n")
                 continue
-
+    
             command, value = line.split("|", 1)
-
+    
             if command == "SCROLL":
-
+    
                 display.scroll(value)
-
+    
                 uart.write("OK|SCROLL\n")
-
+    
             elif command == "TABLE_OCCUPIED":
-
+    
                 zone, size = value.split("|")
-
+    
                 display.scroll("Zone: {}, Size: {}".format(zone, size))
-
+    
                 uart.write("OK|TABLE_OCCUPIED\n")
-
+    
             elif command == "LED":
-
+    
                 if value == "GREEN":
                     display.show(Image.YES)
-
+    
                 elif value == "RED":
                     display.show(Image.NO)
-
+    
                 uart.write("OK|LED\n")
-
+    
             else:
-
+    
                 uart.write("ERROR|Unknown command\n")
+
+def runfan():
+    if distance_sensor.get_distance() < 25:
+        pin2.write_digital(1)
+    else:
+        pin2.write_digital(0)
+
+while True:
+    serialconnection()
+    runfan()
